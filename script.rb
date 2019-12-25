d5c7e8108964fff16eadcdb394863b598e14ebeb
Pavel Lobashov <ShockwaveNN@gmail.com>
2019-12-25 18:11:42 +0300

    Ability to set username (#2)

--- script.rb
+++ script.rb
@@ -2,10 +2,11 @@
 
 require 'onlyoffice_webdriver_wrapper'
 
-url = ENV['URL'] || 'http://178.128.186.166:8000/open?key=f8ba44f2-1fb8-41cb-80c7-5aaa619fbbfa'
+server_url = ENV['URL'] || 'http://178.128.186.166:8000/open?key=f8ba44f2-1fb8-41cb-80c7-5aaa619fbbfa'
 
+url_with_username = "#{server_url}&username=#{Socket.gethostname}"
 chrome = OnlyofficeWebdriverWrapper::WebDriver.new(:chrome)
-chrome.open(url)
+chrome.open(url_with_username)
 loop do
   OnlyofficeLoggerHelper.log("Browser logs: #{chrome.browser_logs}")
   sleep(10)
