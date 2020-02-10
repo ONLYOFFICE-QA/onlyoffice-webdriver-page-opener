7df4cf317f503705d2c48688f8c1e46da64ebb4e
Pavel Lobashov <ShockwaveNN@gmail.com>
2020-02-10 15:15:32 +0300

    Use gem from github source (#12)

--- gems.rb
+++ gems.rb
@@ -2,7 +2,7 @@
 
 source 'https://rubygems.org'
 
-gem 'onlyoffice_webdriver_wrapper'
+gem 'onlyoffice_webdriver_wrapper', git: 'https://github.com/onlyoffice-testing-robot/onlyoffice_webdriver_wrapper'
 
 group :development do
   gem 'overcommit'
