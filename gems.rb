162298d0854a7558a336f06c4e4f3d57e070170f
Pavel Lobashov <shockwavenn@gmail.com>
2020-02-28 11:27:10 +0300

    Actualize dependencies

--- gems.rb
+++ gems.rb
@@ -2,7 +2,7 @@
 
 source 'https://rubygems.org'
 
-gem 'onlyoffice_digitalocean_wrapper', git: 'https://github.com/onlyoffice-testing-robot/onlyoffice_digitalocean_wrapper'
+gem 'onlyoffice_digitalocean_wrapper'
 gem 'onlyoffice_documentserver_testing_framework', git: 'https://github.com/onlyoffice-testing-robot/onlyoffice_documentserver_testing_framework'
 
 group :development do
